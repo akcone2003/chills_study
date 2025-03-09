cb1531c9f98ea488b726f2827fd85233d5e150b0
Aidan Cone <aidandacone@gmail.com>
2025-03-08 16:14:32 -0800

    added CD to app.py

--- app.py
+++ app.py
@@ -107,7 +107,7 @@ if input_file is not None:
                             "PHQ-9", "Sheehan_Disability_Scale_(SDS)", "Brief_Symptom_Inventory-18_(BSI-18)", 
                             "Dispositional_Hope_Scale_(DHS)", "General_Self-Efficacy_Scale_(GSES)", "Subjective_Vitality_Scale_(SVS)",
                             "Flow_State_Scale_(FFS)_(short-version)", "Purpose_In_Life_(PSS)", "Perceived_Stress_Scale_(PSS)",
-                            "Multidimensional_Health_Locus_of_Control_(MHLC)"]
+                            "Multidimensional_Health_Locus_of_Control_(MHLC)", "Connor-Davidson_Resilience_Scale_(CD-RISC-10)"]
 
         selected_scales = st.multiselect(
             "Select scales to analyze:",
